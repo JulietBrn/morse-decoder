0a55ba0f0c1ed56f63fd36a52ffcc0cc3115fcc6
unknown <shishkina.ja@gmail.com>
2023-08-07 11:32:54 +0200

    feat: comlete task

--- src/index.js
+++ src/index.js
@@ -35,10 +35,20 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let newArr =[]
+    let result = []
+    for(let i = 0; i< expr.length; i+=10) {
+        newArr.push(expr.slice(i,i+10))
+    }
+    let sos = newArr.map(val => val.replaceAll('11', '-').replaceAll('10', '.').replaceAll('**********', ' ').replaceAll('0', ''))
+    sos.map(val => {
+        result.push(MORSE_TABLE[val]) 
+    })  
+    return result.join('')
 }
 
 module.exports = {
